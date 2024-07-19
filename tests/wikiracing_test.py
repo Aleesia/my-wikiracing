89622fd121bd736f3e541d60bc984dcf6392ef84
Alisa <48089816+Aleesia@users.noreply.github.com>
2024-07-19 13:20:09 +0300

    Update wikiracing_test.py

--- tests/wikiracing_test.py
+++ tests/wikiracing_test.py
@@ -10,26 +10,22 @@ class WikiRacerTest(unittest.TestCase):
         path = self.racer.find_path('Дружба', 'Рим')
         self.assertEqual(path, ['Дружба', 'Якопо Понтормо', 'Рим'])
 
-    # def test_2(self):
-    #     path = self.racer.find_path('Мітохондріальна ДНК', 'Вітамін K')
-    #     self.assertEqual(path, ['Мітохондріальна ДНК', 'Дезоксирибонуклеїнова кислота', 'Аденозинтрифосфат', 'Вітамін K'])
+    def test_2(self):
+        path = self.racer.find_path('Мітохондріальна ДНК', 'Вітамін K')
+        self.assertEqual(path, ['Мітохондріальна ДНК', 'Дезоксирибонуклеїнова кислота', 'Аденозинтрифосфат', 'Вітамін K'])
 
-    # def test_3(self):
-    #     path = self.racer.find_path('Марка (грошова одиниця)', 'Китайський календар')
-    #     self.assertEqual(path, ['Марка (грошова одиниця)', 'Європа', 'Хвилина', 'Китайський календар'])
+    def test_3(self):
+        path = self.racer.find_path('Марка (грошова одиниця)', 'Китайський календар')
+        self.assertEqual(path, ['Марка (грошова одиниця)', 'Європа', 'Хвилина', 'Китайський календар'])
 
-    # def test_4(self):
-    #     path = self.racer.find_path('Фестиваль', 'Пілястра')
-    #     self.assertEqual(path, ['Фестиваль', 'Бароко', 'Пілястра'])
+    def test_4(self):
+        path = self.racer.find_path('Фестиваль', 'Пілястра')
+        self.assertEqual(path, ['Фестиваль', 'Бароко', 'Пілястра'])
 
     def test_5(self):
         path = self.racer.find_path('Дружина (військо)', '6 жовтня')
         self.assertEqual(path, ['Дружина (військо)', 'Велике князівство Московське', '22 жовтня', '6 жовтня'])
 
-    def test_6(self):
-        path = self.racer.find_path('Натяжні стелі', 'Мошногір\'я')
-        print("found path from Натяжні стелі to Мошногір\'я: ")
-        print(path)
 
 if __name__ == '__main__':
     unittest.main()
