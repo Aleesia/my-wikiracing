e06f0dc8d7e6a909905503809086075939473e81
Alisa <48089816+Aleesia@users.noreply.github.com>
2024-07-19 21:05:06 +0300

    Update wikiracing.py

--- src/wikiracing.py
+++ src/wikiracing.py
@@ -59,8 +59,8 @@ class WikiRacer:
         except Exception:
             self.conn.commit()
             mylist = [str(i) for i in range(1, max_path_length + 1)]
-            columns = " varchar(255), column_".join(mylist)
-            columns = "column_" + columns + " varchar(255)"
+            columns = " varchar(255), page_".join(mylist)
+            columns = "page_" + columns + " varchar(255)"
             query = f"CREATE TABLE {self.db_table} ({columns});"
             print("Create Table query: ", query)
             self.cursor.execute(query)
