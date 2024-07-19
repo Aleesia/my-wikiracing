d6097beb7426d57df7b0a01587fdbdfdb82d2795
Alisa <48089816+Aleesia@users.noreply.github.com>
2024-07-19 23:59:49 +0300

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,14 +1,26 @@
-SELECT TOP 5 articles_links_to_them FROM (COUNT parent_page GROUP BY curr_page ORDER BY num_links_to_page);
+-- top 5 що мають найбільшу кількість посилань на себе
 
-SELECT (page_1, page2) AS (parent, child) FROM wikipages WHERE page_2 IS NOT NONE
-SELECT (page_2, page3) FROM wikipages WHERE page_3 IS NOT NONE
-SELECT (page_3, page4) FROM wikipages WHERE page_4 IS NOT NONE
-SELECT (page_4, page5) FROM wikipages WHERE page_5 IS NOT NONE
-
+SELECT child, count(child) as num_links_to
+  FROM  
+((SELECT page_1 AS parent, page_2 AS child FROM wikipages WHERE (page_2 IS NOT NONE AND page_3 IS NONE))
+  UNION
+(SELECT page_2 AS parent, page_3 AS child FROM wikipages WHERE (page_3 IS NOT NONE AND page_4 IS NONE))
+  UNION
+(SELECT page_3 AS parent, page_4 AS child FROM wikipages WHERE (page_4 IS NOT NONE AND page_5 IS NONE))
+  UNION
+(SELECT page_4 AS parent, page_5 AS child FROM wikipages WHERE page_5 IS NOT NONE))
+ GROUP by child
+  ORDER BY num_links_to DESC
   
-SELECT TOP 5 articles_with_out_links
-
-SELECT (page_1, page2) AS (parent, child) FROM wikipages WHERE page_2 IS NOT NONE
-SELECT (page_2, page3) FROM wikipages WHERE page_3 IS NOT NONE
-SELECT (page_3, page4) FROM wikipages WHERE page_4 IS NOT NONE
-SELECT (page_4, page5) FROM wikipages WHERE page_5 IS NOT NONE
+-- top 5  з найбільшою кількістю посилань на інші статті
+SELECT parent, count(parent) as num_links_from
+  FROM  
+((SELECT page_1 AS parent, page_2 AS child FROM wikipages WHERE (page_2 IS NOT NONE AND page_3 IS NONE))
+  UNION
+(SELECT page_2 AS parent, page_3 AS child FROM wikipages WHERE (page_3 IS NOT NONE AND page_4 IS NONE))
+  UNION
+(SELECT page_3 AS parent, page_4 AS child FROM wikipages WHERE (page_4 IS NOT NONE AND page_5 IS NONE))
+  UNION
+(SELECT page_4 AS parent, page_5 AS child FROM wikipages WHERE page_5 IS NOT NONE))
+ GROUP by parent
+  ORDER BY num_links_from DESC
