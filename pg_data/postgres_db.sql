04abd61c3dca430bd136c8583546f70889823efb
Alisa <48089816+Aleesia@users.noreply.github.com>
2024-07-19 13:20:52 +0300

    Update postgres_db.sql

--- pg_data/postgres_db.sql
+++ pg_data/postgres_db.sql
@@ -4,15 +4,5 @@ CREATE TABLE wikipages
     page_2 VARCHAR(255),
     page_3 VARCHAR(255),
     page_4 VARCHAR(255),
-    page_5 VARCHAR(255),
-    page_6 VARCHAR(255),
-    page_7 VARCHAR(255),
-    page_8 VARCHAR(255),
-    page_9 VARCHAR(255),
-    page_10 VARCHAR(255),
-    page_11 VARCHAR(255),
-    page_12 VARCHAR(255),
-    page_13 VARCHAR(255),
-    page_14 VARCHAR(255),
-    page_15 VARCHAR(255)
+    page_5 VARCHAR(255)
 );
