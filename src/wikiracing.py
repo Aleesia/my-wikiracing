b2ab7b2e539c6783768cdaca298e0d1a5b348bb9
Alisa <48089816+Aleesia@users.noreply.github.com>
2024-07-24 13:37:25 +0300

    Update wikiracing.py

--- src/wikiracing.py
+++ src/wikiracing.py
@@ -71,15 +71,16 @@ class WikiRacer:
         return result
 
     def add_one_page_to_db(self, page: str, next_one: str) -> None:
-        self.cursor.execute("""
-            INSERT INTO wikipages (parent, child)
-            VALUES (%s, %s);""", (page, next_one))
-        self.conn.commit()
-        # check
-        self.cursor.execute("""
-            SELECT * FROM wikipages WHERE child = %s;""", (next_one,))
-        res = self.cursor.fetchall()
-        print("INSERT... page = ", page, "next_one = ", next_one, "\n...result = ", res)
+        if not self.child_in_db(next_one):
+            self.cursor.execute("""
+                INSERT INTO wikipages (parent, child)
+                VALUES (%s, %s);""", (page, next_one))
+            self.conn.commit()
+            # check
+            self.cursor.execute("""
+                SELECT * FROM wikipages WHERE child = %s;""", (next_one,))
+            res = self.cursor.fetchall()
+            print("INSERT... page = ", page, "next_one = ", next_one, "\n...result = ", res)
 
     def get_next_from_db(self, start: str) -> List[str]:
         self.cursor.execute("""
