44fee898588faee48c264300872978548501bb83
Alisa <48089816+Aleesia@users.noreply.github.com>
2024-07-24 16:26:30 +0300

    Update wikiracing.py

--- src/wikiracing.py
+++ src/wikiracing.py
@@ -43,6 +43,9 @@ class WikiRacer:
         start = re.sub(' ', '_', start)
         self.path_len = 2
         curr_all_pages = self.get_next_pages_one(start)
+        print("Initial next pages: ")
+        for p in curr_all_pages:
+            print("ini...", start, " : : : ", p)
         while self.finish not in curr_all_pages:
             self.path_len += 1
             curr_all_pages = self.get_next_pages(curr_all_pages)
@@ -112,6 +115,7 @@ class WikiRacer:
     def get_next_pages_one(self, start: str) -> List[str]:
         if self.parent_in_db(start):
             next_pages = self.get_next_from_db(start)
+            print("from db: next = ", next_pages)
         else:
             all_links = self.get_next_links(start)
             next_pages = []
@@ -120,6 +124,7 @@ class WikiRacer:
                 if not self.child_in_db(curr_page):
                     next_pages.append(curr_page)
             self.add_pages_to_db(start, next_pages)
+            print("from links: next = ", next_pages)
         return next_pages
 
     @sleep_and_retry
